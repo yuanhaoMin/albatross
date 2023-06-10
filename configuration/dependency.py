527a15db309962c5e39e22347f766b34260d9159
Yuanhao <yuanhao21@outlook.com>
2023-06-10 20:38:16 +0800

    add csv demo

--- configuration/dependency.py
+++ configuration/dependency.py
@@ -4,8 +4,10 @@ google-search-results==2.4.2
 python-multipart==0.0.6
 """
 import dotenv
+import matplotlib
 import openai
 import pyodbc
 import sqlalchemy
+import tabulate
 import tiktoken
 import uvicorn
